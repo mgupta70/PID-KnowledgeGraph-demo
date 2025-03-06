e0d90e08255bd3adfa7bb6978740a3531482574c
mgupta70 <mgupta70@asu.edu>
2025-03-06 12:52:21 -0800

    added background

--- app.py
+++ app.py
@@ -15,6 +15,33 @@ from helpers import *
 from qa_system import *
 os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
 
+
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('media/KG_neo4j.png')
+
+
+
 #############
 ## Page Set up
 #############
@@ -81,7 +108,6 @@ example_selector = SemanticSimilarityExampleSelector.from_examples(
     FAISS, # Chroma (old): VectorStore class that is used to store the embeddings
     k=3, # number of examples to produce.
 )
-
 
 
 ##########################

346e5101089f91fc6ee8f2d5a29ce2cc5162e558
mgupta70 <mgupta70@asu.edu>
2025-03-06 14:49:24 -0800

    rag step4 added

--- app.py
+++ app.py
@@ -102,51 +102,12 @@ sample_questions = ["What is total number of class 10 symbols?",
 # Let the user select or enter a custom question
 user_question = st.selectbox("See example questions or enter your own:", ["I want to a add a question"] + sample_questions)
 
-# # If the user wants to enter a question, show a text input field
-# if user_question == "I want to a add a question":
-#     col1, col2 = st.columns([1, 6])
-#     with col2:
-#         annotated_text(("Enter your question:", "", "#fea"))
-#         user_question = st.text_input("")
-
-# if user_question:
-#     # select few-shot examples dynamically
-#     selected_fewshot_examples = example_selector.select_examples({"question": user_question})
-#     # pass the user question and few-shot examples to the model
-#     messages = [SystemMessage(f"{system_prompt_for_generating_cypher}"),
-#                 SystemMessage(f"Here is few examples of questions and their corresponding Cypher queries: {selected_fewshot_examples}."),
-#                 HumanMessage(f"{user_question}")]
-#     # generate cypher query
-#     cypher_generated = cypher_generating_model(messages).content
-#     st.write(f"User query converted to: {cypher_generated}")
-#     # run the generated cypher query on the graph
-#     try:
-#         result = run_query(cypher_generated, pidKG)
-#         output_text = "\n".join(str(record) for record in result)
-#         if output_text is not None:
-#             st.text_area("Query Results", output_text, height=100)
-#         else:
-#             output_text = "Unable to get the results! Maybe the question is not right or is out-of-scope.If you think the question is valid, please try rephrasing it."
-#             st.text_area("Query Results", output_text, height=100)
-#     except Exception as e:
-#         output_text = "Unable to get the results! Maybe the question is not right or is out-of-scope.If you think the question is valid, please try rephrasing it."
-#         st.text_area("Query Results", output_text, height=100)
-        
-#     # result = run_query(cypher_generated, pidKG)
-#     # output_text = "\n".join(str(record) for record in result)
-#     # st.text_area("Query Results", output_text, height=100)
-
-graph = Neo4jGraph(
-    url=st.secrets["neo4j_uri"],
-    username=st.secrets["neo4j_user"],
-    password=st.secrets["neo4j_password"],
-    enhanced_schema=True,
-)
-
-from langchain.chains import GraphCypherQAChain
-chain = GraphCypherQAChain.from_llm(cypher_generating_model, graph=graph, verbose=True, allow_dangerous_requests=True)
-
-
+# If the user wants to enter a question, show a text input field
+if user_question == "I want to a add a question":
+    col1, col2 = st.columns([1, 6])
+    with col2:
+        annotated_text(("Enter your question:", "", "#fea"))
+        user_question = st.text_input("")
 
 if user_question:
     # select few-shot examples dynamically
@@ -160,15 +121,15 @@ if user_question:
     st.write(f"User query converted to: {cypher_generated}")
     # run the generated cypher query on the graph
     try:
-        result = chain.run(cypher_generated)
-        st.text_area("Query Results", result, height=100)
-        # result = run_query(cypher_generated, pidKG)
-        # output_text = "\n".join(str(record) for record in result)
-        # if output_text is not None:
-        #     st.text_area("Query Results", output_text, height=100)
-        # else:
-        #     output_text = "Unable to get the results! Maybe the question is not right or is out-of-scope.If you think the question is valid, please try rephrasing it."
-        #     st.text_area("Query Results", output_text, height=100)
+        result = run_query(cypher_generated, pidKG)
+        output_text = "\n".join(str(record) for record in result)
+        qa_messages = [HumanMessage(f"Question is {user_question} and the answer by running cypher query is {result}: Generate a human readable answer for the question.")]
+        response = cypher_generating_model(qa_messages).content # llm is same as cypher_generating_model
+        if output_text is not None:
+            st.text_area("Query Results", response, height=100)
+        else:
+            output_text = "Unable to get the results! Maybe the question is not right or is out-of-scope.If you think the question is valid, please try rephrasing it."
+            st.text_area("Query Results", output_text, height=100)
     except Exception as e:
         output_text = "Unable to get the results! Maybe the question is not right or is out-of-scope.If you think the question is valid, please try rephrasing it."
         st.text_area("Query Results", output_text, height=100)
@@ -177,8 +138,6 @@ if user_question:
     # output_text = "\n".join(str(record) for record in result)
     # st.text_area("Query Results", output_text, height=100)
 
-
-    
     
 st.markdown("""
     <div style="

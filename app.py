a63f8bd08b8a4734ce7c01565a15b50766057ea6
mgupta70 <mgupta70@asu.edu>
2025-03-06 19:33:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ if user_question:
 # Footer
 st.markdown(
     '''
-    :man-raising-hand: Drop an email with your feedback or suggestions to add more features related to P&ID QA System via email...  :email: Contact me at : mgupta70@asu.edu || [Mohit Gupta](https://mgupta70.github.io), Ph.D., Arizona State University
+    :man-raising-hand: Drop an email with your feedback or suggestion for more useful more features related to P&ID processing ...  :email: Contact me at : mgupta70@asu.edu ([Mohit Gupta](https://mgupta70.github.io), Ph.D., Arizona State University)
     '''
 )
 
